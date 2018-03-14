ec8e57a024e8cd605f26a14d097baae59af9f483
Antero Maripuu <antero.maripuu@gmail.com>
2018-03-14 19:33:01 +0000

    Programming for Everybody homework
    Signed-off-by: machinelearningxl <antero.maripuu@gmail.com>

--- 01. Programming for Everybody-Getting Started with Python/Assignment 2.2.py
+++ 01. Programming for Everybody-Getting Started with Python/Assignment 2.2.py
@@ -1,5 +1,6 @@
 # Write a program that uses raw_input to prompt a user for their name and then
 #welcomes them. Note that raw_input will pop up a dialog box.
 
+
 inp=input("Please enter your name : ") #Phyton 3 function input(), Python 2.7 raw_input()
 print ("Your name is: ", inp)
