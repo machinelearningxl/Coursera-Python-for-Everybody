7394506cfe8d1ae95e62f9abb9863ab041aaa484
machinelearningxl <antero.maripuu@gmail.com>
2018-03-14 21:58:43 +0000

    Certificate Capstone Retrieving, Processing, and Visualizing Data with Python
    Signed-off-by: machinelearningxl <antero.maripuu@gmail.com>

---
+++ 02. Python Data Structures/Assignment 6.5.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+#@Filename : Assignment 6.5
+#@Date : 2018-03-14-21-57
+#@Poject: Coursera-Python-for-Everybody
+#@AUTHOR : Antero Maripuu
+
+# Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
+#Convert the extracted value to a floating point number and print it out.
