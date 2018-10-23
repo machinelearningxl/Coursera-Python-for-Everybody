cadbd22c8b1900f7cb0ac1a213ef0caaa9f99e27
Antero Maripuu <antero.maripuu@gmail.com>
2018-10-23 14:39:47 +0100

    UPdate

--- 02. Python Data Structures/Assignment 6.5.py
+++ 02. Python Data Structures/Assignment 6.5.py
@@ -5,5 +5,6 @@
 #@AUTHOR : Antero Maripuu
 
 
-# Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
-#Convert the extracted value to a floating point number and print it out.
+# Write code using find() and string slicing (see section 6.10) to extract the
+# number at the end of the line below. Convert the extracted value to a
+# floating point number and print it out.
--- 02. Python Data Structures/Assignment 7.2.py
+++ 02. Python Data Structures/Assignment 7.2.py
@@ -4,8 +4,18 @@
 #@Poject: Coursera-Python-for-Everybody
 #@AUTHOR : Antero Maripuu
 
-# Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+
+# Write a program that prompts for a file name, then opens that file and reads
+# through the file, looking for lines of the form:
+
 # X-DSPAM-Confidence:    0.8475
-# Count these lines and extract the floating point values from each of the lines and compute the average of those values and
-# produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
-# when you are testing enter mbox-short.txt as the file name.
+
+# Count these lines and extract the floating point values from each of the
+# lines and compute the average of those values and produce an output as shown
+# below. Do not use the sum() function or a variable named sum in your solution.
+
+# You can download the sample data at
+# http://www.pythonlearn.com/code/mbox-short.txt when you are testing below
+# enter mbox-short.txt as the file name.
+
+# Use the file name mbox-short.txt as the file name
