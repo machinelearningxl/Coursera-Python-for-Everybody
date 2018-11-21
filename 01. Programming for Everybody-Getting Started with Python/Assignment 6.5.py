cdceb3c8707b7ebab048d1dc8a97087343eb3c3b
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-21 18:59:39 +0000

    UPdate

--- 01. Programming for Everybody-Getting Started with Python/Assignment 6.5.py
+++ 01. Programming for Everybody-Getting Started with Python/Assignment 6.5.py
@@ -6,6 +6,13 @@
 # @Last modified by:   machinelearningxl
 # @Last modified time: 2017-11-07T14:25:20+00:00
 
-#6.5 Write code using find() and string slicing (see section 6.10) to extract
-#the number at the end of the line below. Convert the extracted value to a
-#floating point number and print it out. text = "X-DSPAM-Confidence:    0.8475" Desired Output: 0.8475
+"""
+6.5 Write code using find() and string slicing (see section 6.10) to extract
+the number at the end of the line below. Convert the extracted value to a
+floating point number and print it out. text = "X-DSPAM-Confidence:    0.8475" Desired Output: 0.8475
+"""
+
+text = "X-DSPAM-Confidence:    0.8475"
+
+position = text.find('0')
+print(float(text[position + 1:]))
