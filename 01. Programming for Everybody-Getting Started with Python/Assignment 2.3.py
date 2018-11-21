c655b54a723dc7be593ac8363e63a2457d094125
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-21 15:03:17 +0000

    UPdate

--- 01. Programming for Everybody-Getting Started with Python/Assignment 2.3.py
+++ 01. Programming for Everybody-Getting Started with Python/Assignment 2.3.py
@@ -7,14 +7,19 @@
 # @Last modified time: 2018-11-16 15:23:38
 
 
+"""
+Write a program to prompt the user for hours and
+rate per hour to compute gross pay.
+"""
 
-#Write a program to prompt the user for hours and
-#rate per hour to compute gross pay.
+inp = float(input("Please enter your hours: "))  # Python 3 function input(), Python 2.7 raw_input()
+inp2 = float(input("Please enter your rate (e.g £20.15): "))
 
-inp=float(input("Please enter your hours: ")) #Phyton 3 function input(), Python 2.7 raw_input()
-inp2=float(input("Please enter your rate (e.g £20.15): "))
 
 def fname(x,y):
-  print(x*y)
+    print(x*y)
 
-print("Your calculated cross pay is "),fname(inp, inp2)
+
+print("Your calculated cross pay is "), fname(inp, inp2)
+
+
--- 01. Programming for Everybody-Getting Started with Python/Assignment 2.5.py
+++ 01. Programming for Everybody-Getting Started with Python/Assignment 2.5.py
@@ -7,14 +7,17 @@
 # @Last modified time: 2018-11-20 13:57:16
 
 
+"""
+2.5 Write a program which prompts the user for a Celsius temperature,
+convert the temperature to Fahrenheit, and print out the converted
+temperature.
+"""
 
-#2.5 Write a program which prompts the user for a Celsius temperature,
-#convert the temperature to Fahrenheit, and print out the converted
-#temperature.
+inp = (input("Please enter a Celsius temperature "))  # Python 3 function input(), Python 2.7 raw_input()
 
-inp=float(input("Please enter a Celsius temperature ")) #Phyton 3 function input(), Python 2.7 raw_input()
 
 def fname(x):
     return x * (9.0/5.0) + 32
+
 
 print("Temperature in Fahrenheit", fname(inp))
