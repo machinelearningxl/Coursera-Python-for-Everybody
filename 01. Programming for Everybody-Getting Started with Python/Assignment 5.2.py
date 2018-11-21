412bcab0955cc75e58f4219100a735845d4885ab
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-21 16:11:32 +0000

    UPdate

--- 01. Programming for Everybody-Getting Started with Python/Assignment 5.2.py
+++ 01. Programming for Everybody-Getting Started with Python/Assignment 5.2.py
@@ -7,9 +7,10 @@
 # @Last modified time: 2018-11-16 15:25:25
 
 
-
-#5.2 Write a program that repeatedly prompts a user for integer numbers until
-#the user enters 'done'. Once 'done' is entered, print out the largest and
-#smallest of the numbers. If the user enters anything other than a valid number
-#catch it with a try/except and put out an appropriate message and ignore the
-#number. Enter the numbers from the book for problem 5.1 and Match the desired output as showen.
+"""
+5.2 Write a program that repeatedly prompts a user for integer numbers until
+the user enters 'done'. Once 'done' is entered, print out the largest and
+smallest of the numbers. If the user enters anything other than a valid number
+catch it with a try/except and put out an appropriate message and ignore the
+number. Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
+"""
