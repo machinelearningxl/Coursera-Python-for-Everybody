8c0bc8dcad62e77e812cf4bf1cb651b7589ffb32
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-23 16:07:33 +0000

    UPdate

--- 03. Using Python to Access Web Data/Week 2 - Regular Expressions/Assignment_regular_expressions.py
+++ 03. Using Python to Access Web Data/Week 2 - Regular Expressions/Assignment_regular_expressions.py
@@ -4,7 +4,9 @@
 # @Project: Coursera
 # @Filename : Assignment_regular_expressions
 
-
-
+"""
+In this assignment you will read through and parse a file with text and numbers.
+You will extractall the numbers in the file and compute the sum of the numbers.
+"""
 
 
