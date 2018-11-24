3659a98b6c3325194703d8405eebf205d875f640
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-24 23:45:53 +0000

    UPdate

---
+++ 03. Using Python to Access Web Data/Week 4 -  BeautifulSoup/Assignment_2.py
@@ -0,0 +1,23 @@
+# @Author: Antero Maripuu Github:<machinelearningxl>
+# @Date : 2018-11-24 23:42
+# @Email:  antero.maripuu@gmail.com
+# @Project: Coursera
+# @Filename : Assignment_2.py
+
+"""
+In this assignment you will write a Python program that expands on http://www.pythonlearn.com/code/urllinks.py
+The program will use urllib to read the HTML from the data files below, extract
+the href= vaues from the anchor tags, scan for a tag that is in a particular
+position from the top and follow that link, repeat the process a number of times,
+and report the last name you find.
+SAMPLE:
+Find the link at position 3 (the first name is 1). Follow that link. Repeat this
+process 4 times. The answer is the last name that you retrieve.
+The result should be: Anayah
+PROBLEM:
+Find the link at position 18 (the first name is 1). Follow that link. Repeat this
+process 7 times. The answer is the last name that you retrieve.
+Hint: the name starts with S
+"""
+
+
