3deffcdb84718b8bcbb84ddbadca5557cc67d01b
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-24 00:19:32 +0000

    UPdate

--- 03. Using Python to Access Web Data/Week 2 - Regular Expressions/Assignment_regular_expressions.py
+++ 03. Using Python to Access Web Data/Week 2 - Regular Expressions/Assignment_regular_expressions.py
@@ -9,4 +9,24 @@ In this assignment you will read through and parse a file with text and numbers.
 You will extractall the numbers in the file and compute the sum of the numbers.
 """
 
+import re
+
+words_file = input("Type a file name for processing: ")
+if words_file == "":
+    words_file = open("regex text data.txt")
+
+
+su = 0
+for x in words_file:
+    line = x.strip()
+    stuff = re.findall("[0-9]+", x)
+    for y in stuff:
+        y = int(y)
+        su += y
+
+print("Sum of the numbers: ",su)
+
+
+
+
 
