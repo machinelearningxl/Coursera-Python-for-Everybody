62852f63242d8dc83240b8bb087e7428793cfd6b
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-24 21:04:19 +0000

    UPdate

--- 03. Using Python to Access Web Data/Week 3 - Networks and Sockets/Assignment_Networks_and_sockets.py
+++ 03. Using Python to Access Web Data/Week 3 - Networks and Sockets/Assignment_Networks_and_sockets.py
@@ -8,3 +8,33 @@
 Making the HTTP request that will get us the desired document
 http://www.pythonlearn.com/code/intro-short.txt
 """
+
+import socket
+import  sys
+
+host = "data.pr4e.org"
+try:
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+except socket.error:
+    print("Socket creation failed with error")
+
+try:
+    remote_ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+mysock.connect(("data.pr4e.org", 80))
+cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:   #     If no data, break
+        print("No Data ERROR")
+        break
+    else:
+        print("RECIVED:")
+        print(data.decode())
+mysock.close()
