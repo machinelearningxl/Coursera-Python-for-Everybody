c952f7a7aa7303340ee2832dbd1831cb9e477b85
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-24 00:29:18 +0000

    UPdate

---
+++ 03. Using Python to Access Web Data/Week 3 - Networks and Sockets/Assignment_Networks_and_sockets.py
@@ -0,0 +1,10 @@
+# @Author: Antero Maripuu Github:<machinelearningxl>
+# @Date : 2018-11-24 00:28
+# @Email:  antero.maripuu@gmail.com
+# @Project: Coursera
+# @Filename : Assignment_Networks_and_sockets.py
+
+"""
+Making the HTTP request that will get us the desired document
+http://www.pythonlearn.com/code/intro-short.txt
+"""
