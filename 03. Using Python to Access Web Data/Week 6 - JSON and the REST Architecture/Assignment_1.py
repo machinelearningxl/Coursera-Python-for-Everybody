df94db5b7a9cde5ada35958b3e922ad9f770c3a7
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-25 00:36:07 +0000

    UPdate

---
+++ 03. Using Python to Access Web Data/Week 6 - JSON and the REST Architecture/Assignment_1.py
@@ -0,0 +1,10 @@
+# @Author: Antero Maripuu Github:<machinelearningxl>
+# @Date : 2018-11-25 00:34
+# @Email:  antero.maripuu@gmail.com
+# @Project: Coursera
+# @Filename : Assignment_1.py
+
+
+
+
+
---
+++ 03. Using Python to Access Web Data/Week 6 - JSON and the REST Architecture/Assignment_2.py
@@ -0,0 +1,10 @@
+# @Author: Antero Maripuu Github:<machinelearningxl>
+# @Date : 2018-11-25 00:35
+# @Email:  antero.maripuu@gmail.com
+# @Project: Coursera
+# @Filename : Assignment_2.py
+
+
+
+
+
