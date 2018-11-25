252eec2fed8344102d0d34b340682697c90c8c68
Antero Maripuu <antero.maripuu@gmail.com>
2018-11-25 18:55:47 +0000

    UPdate

---
+++ 04. Using Databases with Python/Week 2 - Counting Email in a Database/Assignment_1.py
@@ -0,0 +1,12 @@
+# @Author: Antero Maripuu Github:<machinelearningxl>
+# @Date : 2018-11-25 18:54
+# @Email:  antero.maripuu@gmail.com
+# @Project: Coursera
+# @Filename : Assignment_1.py
+
+"""
+create db, create table (org, count) and count number emails per organization
+
+"""
+
+
